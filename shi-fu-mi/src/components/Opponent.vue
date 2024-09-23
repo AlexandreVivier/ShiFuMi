73338a6679fe85ea6f22bbf3f753cec8989dbbcf
Krill <80386194+AlexandreVivier@users.noreply.github.com>
2024-09-23 20:27:28 +0200

    WIP

---
+++ shi-fu-mi/src/components/Opponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <button v-on:click="newGame()"> Recommencer la partie </button>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        newGame() {
+            this.$emit('newGame');
+        }
+    }
+}
+</script>
---
+++ shi-fu-mi/src/components/Player.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <button v-on:click="rockChosen()">Pierre</button>
+        <button v-on:click="paperChosen()">Feuille</button>
+        <button v-on:click="scissorsChosen()">Ciseaux</button>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        rockChosen() {
+            this.$emit('getChoice', 'rock');
+        },
+        paperChosen() {
+            this.$emit('getChoice', 'paper');
+        },
+        scissorsChosen() {
+            this.$emit('getChoice', 'scissors');
+        }
+    }
+}
+</script>
